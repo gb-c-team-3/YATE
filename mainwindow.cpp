321e0508572a1e6871ed12672ab8ee7805268f13
Stepan <Steven.nnov@gmail.com>
2023-04-20 13:02:07 +0300

    T1 - created project
    Created QT project - main window with Text Browser vidget

---
+++ mainwindow.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle(tr("Team-3 Text Editor"));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
