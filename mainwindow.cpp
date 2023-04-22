bad49d97d92ea2ce7596105f013ce8da7f245a75
Irina <irina21022012@gmail.com>
2023-04-22 18:17:47 +0300

    Irina

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -14,3 +18,52 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_actionOpen_triggered()
+{
+    QString file_name = QFileDialog::getOpenFileName(this, "Open the file");
+    QFile file(file_name);
+    file_path = file_name;
+    if(!file.open(QFile::ReadOnly | QFile::Text))
+    {
+        QMessageBox::warning(this, "Warning", "Cannot open the file");
+        return;
+    }
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+
+void MainWindow::on_actionSave_triggered()
+{
+    QFile file(file_path);
+    if(!file.open(QFile::WriteOnly | QFile::Text))
+    {
+        QMessageBox::warning(this, "Warning", "Cannot save the file");
+        return;
+    }
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.flush();
+    file.close();
+}
+
+
+void MainWindow::on_actionSave_as_triggered()
+{
+    QString file_name = QFileDialog::getSaveFileName(this, "Save the file");
+    QFile file(file_name);
+    if(!file.open(QFile::WriteOnly | QFile::Text))
+    {
+        QMessageBox::warning(this, "Warning", "Cannot save the file");
+        return;
+    }
+    file_path = file_name;
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.flush();
+    file.close();
+}
