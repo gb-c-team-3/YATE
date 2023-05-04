b33b2b0beb3246f33be59a9d970a87cd2363e94c
Irina <irina21022012@gmail.com>
2023-05-04 15:21:45 +0300

    T9 - offer your own interface
    1. Исправила ошибку "open file" функцию в .cpp файле, которая появилась в результате слияния кода меню и кода функций: "file_name = file_name" -> "file_path = file_name", а также добавила "QString file_path = ""; " в .h файл ;
    2. В меню исправила сочетание клавиш: Ctrl+N -> Ctrl+O, потому что Ctrl+N как правило используется для "New document";
    3. Добавила вкладки меню: "Edit", "Format", "Insert", "View".
    4. Добавила подпункты меню: "File->New->Open->Save->Save as->Print->Exit", "Edit->Undo->Redo->Copy->Cut->Paste->SelectAll", "Format->Bold->Italic->Underlined->Crossed->Font Style->Font color", "Insert->Image->Table", "View->Dark Mode->Light Mode";
    5. Добавила toolbar и qAction для каждого компонента toolbar: "New", "Open", "Save", "Save as", "Print", "Undo", "Redo", "Copy", "Cut", "Paste", "Bold", "Italic", "Underlined", "Font style", "Font color", "Insert iamge", "Insert table";
    6. Добавила слоты для каждой функции;
    7. Создала resource file в который поместила все иконки, белые для темной темы и темно серые для белой темы;
    8. Изменила шрифт приложения и меню;
    9. Добавила иконки во все пункты меню, иконки все содержатся в resource file;
    10. Добавила иконки во все QActions toolbar.

--- TextEditor.cpp
+++ TextEditor.cpp
@@ -4,13 +4,27 @@
 #include <QFileDialog>
 #include <QTextStream>
 #include <QMessageBox>
+#include <QFont>
+#include <QFontDialog>
+#include <QColorDialog>
+#include <QColor>
+#include <QImage>
+#include <QIcon>
+#include <QToolBar>
+#include <QAction>
 
 
 TextEditor::TextEditor(QWidget *parent)
     : QMainWindow(parent), uiPtr(new Ui::TextEditor)
 {
     uiPtr->setupUi(this);
+    uiPtr->menubar->setFont(tr("Corbel"));
     uiPtr->menubar->addMenu(menuConfig());
+    uiPtr->menubar->addMenu(editMenu());
+    uiPtr->menubar->addMenu(formatMenu());
+    uiPtr->menubar->addMenu(insertMenu());
+    uiPtr->menubar->addMenu(viewMenu());
+    uiPtr->toolBar->addWidget(toolbar());
     setWindowTitle(tr("Team-3 Text Editor"));
 }
 
@@ -19,11 +33,16 @@ TextEditor::~TextEditor()
     delete uiPtr;
 }
 
+void TextEditor::slotFileNew()
+{
+
+}
+
 void TextEditor::slotFileOpen()
 {
     QString file_name = QFileDialog::getOpenFileName(this, "Open the file");
     QFile file(file_name);
-    file_name = file_name;
+    file_path = file_name;
     if(!file.open(QFile::ReadOnly | QFile::Text))
     {
         QMessageBox::warning(this, "Warning", "Cannot open the file");
@@ -79,17 +98,219 @@ void TextEditor::slotPrintFile()
     uiPtr->textBrowser->print(&printer);
 }
 
+void TextEditor::slotExitFile()
+{
+
+}
+
+void TextEditor::slotUndo()
+{
+
+}
+
+void TextEditor::slotRedo()
+{
+
+}
+
+void TextEditor::slotCopy()
+{
+
+}
+
+void TextEditor::slotCut()
+{
+
+}
+
+void TextEditor::slotPaste()
+{
+
+}
+
+void TextEditor::slotSelectAll()
+{
+
+}
+
+void TextEditor::slotBold()
+{
+
+}
+
+void TextEditor::slotItalic()
+{
+
+}
+
+void TextEditor::slotUnderlined()
+{
+
+}
+
+void TextEditor::slotCrossedOut()
+{
+
+}
+
+void TextEditor::slotFontStyle()
+{
+
+}
+
+void TextEditor::slotFontColor()
+{
+
+}
+
+void TextEditor::slotInsertImage()
+{
+
+}
+
+void TextEditor::slotInsertTable()
+{
+
+}
+
+void TextEditor::slotDarkMode()
+{
+
+}
+
+void TextEditor::slotLightMode()
+{
+
+}
+
 QMenu *TextEditor::menuConfig()
 {
     QMenu *menuFilePtr = new QMenu(this);
-    menuFilePtr->setTitle(tr("Files"));
-    menuFilePtr->addAction(tr("Open file"), this, &TextEditor::slotFileOpen, QKeySequence::New); //Ctrl+N
+    menuFilePtr->setFont(tr("Corbel"));
+    menuFilePtr->setTitle(tr("File"));
+    menuFilePtr->addAction(tr("New"), QKeySequence::New, this, &TextEditor::slotFileNew)->setIcon(QIcon(":/res/Icons/file")); //Ctrl+N
+    menuFilePtr->addAction(tr("Open"), QKeySequence::Open, this, &TextEditor::slotFileOpen)->setIcon(QIcon(":/res/Icons/folder")); //Ctrl+O
+    menuFilePtr->addAction(tr("Save"), QKeySequence::Save, this, &TextEditor::slotFileSave)->setIcon(QIcon(":/res/Icons/diskette"));   //Ctrl+S
+    menuFilePtr->addAction(tr("Save as"), QKeySequence::SaveAs, this, &TextEditor::slotFileSaveAs)->setIcon(QIcon(":/res/Icons/save-as"));//Ctrl+Shift+S
+    menuFilePtr->addAction(tr("Print"), QKeySequence::Print, this, &TextEditor::slotPrintFile)->setIcon(QIcon(":/res/Icons/printer")); //Ctrl+P
     menuFilePtr->addSeparator();
-    menuFilePtr->addAction(tr("Save file"), this, &TextEditor::slotFileSave, QKeySequence::Save);   //Ctrl+S
-    menuFilePtr->addSeparator();
-    menuFilePtr->addAction(tr("Save file as"), this, &TextEditor::slotFileSaveAs, QKeySequence::SaveAs);//Ctrl+Shift+S
-    menuFilePtr->addSeparator();
-    menuFilePtr->addAction(tr("Print file"), this, &TextEditor::slotPrintFile, QKeySequence::Print); //Ctrl+P
+    menuFilePtr->addAction(tr("Exit"), this, &TextEditor::slotExitFile)->setIcon(QIcon(":/res/Icons/logout"));
     return menuFilePtr;
+}
+
+QMenu *TextEditor::editMenu()
+{
+    QMenu *menuEditPtr = new QMenu(this);
+    menuEditPtr->setFont(tr("Corbel"));
+    menuEditPtr->setTitle(tr("Edit"));
+    menuEditPtr->addAction(tr("Undo"), QKeySequence::Undo, this, &TextEditor::slotUndo)->setIcon(QIcon(":/res/Icons/turn-left")); //Ctrl+Z
+    menuEditPtr->addAction(tr("Redo"), QKeySequence::Redo, this, &TextEditor::slotRedo)->setIcon(QIcon(":/res/Icons/forward")); //Ctrl+Y
+    menuEditPtr->addAction(tr("Copy"), QKeySequence::Copy, this, &TextEditor::slotCopy)->setIcon(QIcon(":/res/Icons/copy")); //Ctrl+C
+    menuEditPtr->addAction(tr("Cut"),QKeySequence::Cut, this, &TextEditor::slotCut)->setIcon(QIcon(":/res/Icons/scissors")); //Ctrl+X
+    menuEditPtr->addAction(tr("Paste"), QKeySequence::Paste, this, &TextEditor::slotPaste)->setIcon(QIcon(":/res/Icons/paste")); //Ctrl+V
+    menuEditPtr->addSeparator();
+    menuEditPtr->addAction(tr("Select All"), QKeySequence::SelectAll, this, &TextEditor::slotSelectAll)->setIcon(QIcon(":/res/Icons/checkbox")); //Ctrl+A
+    return menuEditPtr;
+}
+
+QMenu *TextEditor::formatMenu()
+{
+    QMenu *menuFormatPtr = new QMenu(this);
+    menuFormatPtr->setFont(tr("Corbel"));
+    menuFormatPtr->setTitle(tr("Format"));
+    menuFormatPtr->addAction(tr("Bold"), QKeySequence::Bold, this, &TextEditor::slotBold)->setIcon(QIcon(":/res/Icons/bold")); //Ctrl+B
+    menuFormatPtr->addAction(tr("Italic"), QKeySequence::Italic, this, &TextEditor::slotItalic)->setIcon(QIcon(":/res/Icons/italic")); //Ctrl+I
+    menuFormatPtr->addAction(tr("Underline"), QKeySequence::Underline, this, &TextEditor::slotUnderlined)->setIcon(QIcon(":/res/Icons/underline")); //Ctrl+U
+    menuFormatPtr->addAction(tr("Crossed"),this, &TextEditor::slotCrossedOut)->setIcon(QIcon(":/res/Icons/cross-out"));
+    menuFormatPtr->addSeparator();
+    menuFormatPtr->addAction(tr("Font style"), this, &TextEditor::slotFontStyle)->setIcon(QIcon(":/res/Icons/font-adjustment"));
+    menuFormatPtr->addAction(tr("Font color"), this, &TextEditor::slotFontColor)->setIcon(QIcon(":/res/Icons/color-text"));
+    return menuFormatPtr;
+}
+
+QMenu *TextEditor::insertMenu()
+{
+    QMenu *menuInsertPtr = new QMenu(this);
+    menuInsertPtr->setFont(tr("Corbel"));
+    menuInsertPtr->setTitle(tr("Insert"));
+    menuInsertPtr->addAction(tr("Image"), this, &TextEditor::slotInsertImage)->setIcon(QIcon(":/res/Icons/insert-picture-icon"));
+    menuInsertPtr->addAction(tr("Table"), this, &TextEditor::slotInsertTable)->setIcon(QIcon(":/res/Icons/tablet"));
+    return menuInsertPtr;
+}
+
+QMenu *TextEditor::viewMenu()
+{
+    QMenu *menuViewPtr = new QMenu(this);
+    menuViewPtr->setFont(tr("Corbel"));
+    menuViewPtr->setTitle(tr("View"));
+    menuViewPtr->addAction(tr("Dark mode"), this, &TextEditor::slotDarkMode)->setIcon(QIcon(":/res/Icons/file"));
+    menuViewPtr->addAction(tr("Light mode"), this, &TextEditor::slotLightMode)->setIcon(QIcon(":/res/Icons/file_white"));
+    return menuViewPtr;
+}
+
+QToolBar *TextEditor::toolbar()
+{
+    QToolBar *toolbar = new QToolBar(this);
+
+    QAction *newdoc = toolbar->addAction(QIcon(":/res/Icons/file"), "New document");
+    connect(newdoc, &QAction::triggered, this, &TextEditor::slotFileNew);
+
+    QAction *open = toolbar->addAction(QIcon(":/res/Icons/folder"), "Open file");
+    connect(open, &QAction::triggered, this, &TextEditor::slotFileOpen);
+
+    QAction *save = toolbar->addAction(QIcon(":/res/Icons/diskette"), "Save file");
+    connect(save, &QAction::triggered, this, &TextEditor::slotFileSave);
+
+    QAction *save_as = toolbar->addAction(QIcon(":/res/Icons/save-as"), "Save file as");
+    connect(save_as, &QAction::triggered, this, &TextEditor::slotFileSaveAs);
+
+    QAction *printdoc = toolbar->addAction(QIcon(":/res/Icons/printer"), "Print document");
+    connect(printdoc, &QAction::triggered, this, &TextEditor::slotPrintFile);
+
+    toolbar->addSeparator();
+
+    QAction *undo = toolbar->addAction(QIcon(":/res/Icons/turn-left"), "Undo");
+    connect(undo, &QAction::triggered, this, &TextEditor::slotUndo);
+
+    QAction *redo = toolbar->addAction(QIcon(":/res/Icons/forward"), "Redo");
+    connect(redo, &QAction::triggered, this, &TextEditor::slotRedo);
+
+    QAction *copy = toolbar->addAction(QIcon(":/res/Icons/copy"), "Copy");
+    connect(copy, &QAction::triggered, this, &TextEditor::slotCopy);
+
+    QAction *cut = toolbar->addAction(QIcon(":/res/Icons/scissors"), "Cut");
+    connect(cut, &QAction::triggered, this, &TextEditor::slotCut);
+
+    QAction *paste = toolbar->addAction(QIcon(":/res/Icons/paste"), "Paste");
+    connect(paste, &QAction::triggered, this, &TextEditor::slotPaste);
+
+    toolbar->addSeparator();
+
+    QAction *bold = toolbar->addAction(QIcon(":/res/Icons/bold"), "Bold");
+    connect(bold, &QAction::triggered, this, &TextEditor::slotBold);
+
+    QAction *italic = toolbar->addAction(QIcon(":/res/Icons/italic"), "Italic");
+    connect(italic, &QAction::triggered, this, &TextEditor::slotItalic);
+
+    QAction *underlined = toolbar->addAction(QIcon(":/res/Icons/underline"), "Underlined");
+    connect(underlined, &QAction::triggered, this, &TextEditor::slotUnderlined);
+
+    toolbar->addSeparator();
+
+    QAction *font_style = toolbar->addAction(QIcon(":/res/Icons/font-adjustment"), "Font style");
+    connect(font_style, &QAction::triggered, this, &TextEditor::slotFontStyle);
+
+    QAction *font_color = toolbar->addAction(QIcon(":/res/Icons/color-text"), "Font color");
+    connect(font_color, &QAction::triggered, this, &TextEditor::slotFontColor);
+
+    toolbar->addSeparator();
+
+    QAction *iamge = toolbar->addAction(QIcon(":/res/Icons/insert-picture-icon"), "Insert image");
+    connect(iamge, &QAction::triggered, this, &TextEditor::slotInsertImage);
+
+    QAction *tablet = toolbar->addAction(QIcon(":/res/Icons/tablet"), "Insert table");
+    connect(tablet, &QAction::triggered, this, &TextEditor::slotInsertTable);
+
+    return toolbar;
 }
 
